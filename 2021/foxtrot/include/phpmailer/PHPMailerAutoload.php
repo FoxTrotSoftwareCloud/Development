d7e3b8ec05153867c510b74557c0d135e15908d3
loren inada <lordawg375@sbcglobal.net>
2022-01-18 21:36:47 -0800

    Update for the latest github refresh

--- 2021/foxtrot/include/phpmailer/PHPMailerAutoload.php
+++ 2021/foxtrot/include/phpmailer/PHPMailerAutoload.php
@@ -42,8 +42,9 @@ if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
      * Fall back to traditional autoload for old PHP versions
      * @param string $classname The name of the class to load
      */
-    function __autoload($classname)
-    {
+    // 10/30/21 __autoload is deprecated use "spl_autoload-register()" instead li
+    function spl_autoload_register($classname) {
+    // function __autoload($classname) {
         PHPMailerAutoload($classname);
     }
 }
