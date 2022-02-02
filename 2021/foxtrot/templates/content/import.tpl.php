5e14d0b3fed5feebfa7bd9c5e01009088e8c6d4a
loren inada <lordawg375@sbcglobal.net>
2022-02-01 22:59:23 -0800

    Change broker & client to uppercase and last, first format

--- 2021/foxtrot/templates/content/import.tpl.php
+++ 2021/foxtrot/templates/content/import.tpl.php
@@ -1314,7 +1314,7 @@ PostResult( msg );
                         <select name="acc_for_client" id="acc_for_client" class="form-control">
                             <option value="">Select Client</option>
                             <?php foreach($get_client as $key=>$val){ ?>
-                            <option value="<?php echo $val['id'];?>"><?php echo $val['first_name'].' '.$val['last_name'];?></option>
+                            <option value="<?php echo $val['id'];?>"><?php echo strtoupper($val['last_name']).((!empty($val['last_name']) AND !empty($val['first_name'])) ? ', ' : '').strtoupper($val['first_name']);?></option>
                             <?php } ?>
                         </select>
                     </div>
