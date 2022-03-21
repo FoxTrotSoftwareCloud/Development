b9d6ee4b9e92698495931a9e6603c6f2034364a0
loren inada <lordawg375@sbcglobal.net>
2022-03-21 10:28:26 -0700

    Minor change to data_interface.php

--- 2021/foxtrot/include/classes/data_interfaces.class.php
+++ 2021/foxtrot/include/classes/data_interfaces.class.php
@@ -73,12 +73,13 @@
         /**
 		 * @param int id
 		 * @return array of record if success, error message if any errors
+		 * $user_id deprecated 3/21/22 - all users should enjoy! This is not Florida!!
 		 * */
-		public function edit($dim_id,$user_id){
+		public function edit($dim_id,$user_id=0){
 			$return = array();
 			$q = "SELECT `at`.*
 					FROM `".$this->table."` AS `at`
-                    WHERE `at`.`is_delete`='0' AND `at`.`dim_id`='".$dim_id."' AND `at`.`user_id`='".$user_id."' ";
+                    WHERE `at`.`is_delete`='0' AND `at`.`dim_id`='".$dim_id."' ";
 			$res = $this->re_db_query($q);
             if($this->re_db_num_rows($res)>0){
     			$return = $this->re_db_fetch_array($res);
