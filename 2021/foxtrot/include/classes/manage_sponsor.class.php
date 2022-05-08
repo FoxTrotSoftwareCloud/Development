00df0c6c6805b1af175df91b2f8ce74e2fb37171
loren inada <lordawg375@sbcglobal.net>
2022-05-07 19:32:16 -0700

    05/07/22 Sponsor Transaction button 	modified:   2021/foxtrot/include/classes/manage_sponsor.class.php 	modified:   2021/foxtrot/manage_sponsor.php 	modified:   2021/foxtrot/templates/content/manage_sponsor.tpl.php

--- 2021/foxtrot/include/classes/manage_sponsor.class.php
+++ 2021/foxtrot/include/classes/manage_sponsor.class.php
@@ -1,19 +1,19 @@
 <?php
 	class manage_sponsor extends db{
-		
+
 		public $errors = '';
-        
+
         /**
 		 * @param post array
 		 * @return true if success, error message if any errors
 		 * */
-		
+
         public function insert_update_sponsor($data)
         {
           $id = isset($data['sponsor_id'])?$this->re_db_input($data['sponsor_id']):0;
           if ($id)
             $originalInstance = $this->select_sponsor_by_id($id);
-          $sponser_name = isset($data['sponser_name'])?$this->re_db_input($data['sponser_name']):'';
+          $sponsor_name = isset($data['sponsor_name'])?$this->re_db_input($data['sponsor_name']):'';
           $saddress1 = isset($data['saddress1'])?$this->re_db_input($data['saddress1']):'';
           $saddress2 = isset($data['saddress2'])?$this->re_db_input($data['saddress2']):'';
           $scity = isset($data['scity'])?$this->re_db_input($data['scity']):'';
@@ -37,8 +37,8 @@
           $for_import = isset($data['for_import'])?$this->re_db_input($data['for_import']):'false';
           $file_id = isset($data['file_id'])?$this->re_db_input($data['file_id']):'';
           $temp_data_id = isset($data['temp_data_id'])?$this->re_db_input($data['temp_data_id']):'';
-            
-          if($sponser_name==''){
+
+          if($sponsor_name==''){
             $this->errors = 'Please enter sponsor name.';
           }
           if($this->errors!=''){
@@ -50,28 +50,28 @@
           if($id>0){
             $con = " AND `id`!='".$id."'";
           }
-          $q = "SELECT * FROM `".SPONSOR_MASTER."` WHERE `is_delete`='0' AND `name`='".$sponser_name."' ".$con;
+          $q = "SELECT * FROM `".SPONSOR_MASTER."` WHERE `is_delete`='0' AND `name`='".$sponsor_name."' ".$con;
           $res = $this->re_db_query($q);
           $return = $this->re_db_num_rows($res);
           if($return>0){
-            $this->errors = 'This sponser is already exists.';
+            $this->errors = 'This sponsor is already exists.';
           }
-			
+
 			if($this->errors!=''){
 				return $this->errors;
 			}
 			else if($id>=0){
 				if($id==0){
-					$q = "INSERT INTO `".SPONSOR_MASTER."` SET `name`='".strtoupper($sponser_name)."',`address1`='".$saddress1."',`address2`='".$saddress2."',`city`='".$scity."',`state`='".$sstate."',`zip_code`='".$szip_code."',`email`='".$semail."',`website`='".$swebsite."',`general_contact`='".$sgeneral_contact."',`general_phone`='".$sgeneral_phone."',`operations_contact`='".$soperations_contact."',`operations_phone`='".$soperations_phone."',`dst_system_id`='".$sdst_system_id."',`dst_mgmt_code`='".$sdst_mgmt_code."',`dst_importing`='".$sdst_import."',`dazl_code`='".$sdazl_code."',`dazl_importing`='".$sdazl_import."',`dtcc_nscc_id`='".$sdtcc_nscc."',`clearing_firm_id`='".$sclr_firm."'".$this->insert_common_sql();
+					$q = "INSERT INTO `".SPONSOR_MASTER."` SET `name`='".strtoupper($sponsor_name)."',`address1`='".$saddress1."',`address2`='".$saddress2."',`city`='".$scity."',`state`='".$sstate."',`zip_code`='".$szip_code."',`email`='".$semail."',`website`='".$swebsite."',`general_contact`='".$sgeneral_contact."',`general_phone`='".$sgeneral_phone."',`operations_contact`='".$soperations_contact."',`operations_phone`='".$soperations_phone."',`dst_system_id`='".$sdst_system_id."',`dst_mgmt_code`='".$sdst_mgmt_code."',`dst_importing`='".$sdst_import."',`dazl_code`='".$sdazl_code."',`dazl_importing`='".$sdazl_import."',`dtcc_nscc_id`='".$sdtcc_nscc."',`clearing_firm_id`='".$sclr_firm."'".$this->insert_common_sql();
 					$res = $this->re_db_query($q);
                     $id = $this->re_db_insert_id();
-                    
+
                     if($for_import == 'true')
                     {
                         $q1 = "UPDATE `".IMPORT_EXCEPTION."` SET `solved`='1' WHERE `file_id`='".$file_id."' and `error_code_id`='14'";
                         $res1 = $this->re_db_query($q1);
                     }
-                        
+
 					if($res){
 					    $_SESSION['success'] = INSERT_MESSAGE;
 						return true;
@@ -82,7 +82,7 @@
 					}
 				}
 				else if($id>0){
-					$q = "UPDATE `".SPONSOR_MASTER."` SET `name`='".strtoupper($sponser_name)."',`address1`='".$saddress1."',`address2`='".$saddress2."',`city`='".$scity."',`state`='".$sstate."',`zip_code`='".$szip_code."',`email`='".$semail."',`website`='".$swebsite."',`general_contact`='".$sgeneral_contact."',`general_phone`='".$sgeneral_phone."',`operations_contact`='".$soperations_contact."',`operations_phone`='".$soperations_phone."',`dst_system_id`='".$sdst_system_id."',`dst_mgmt_code`='".$sdst_mgmt_code."',`dst_importing`='".$sdst_import."',`dazl_code`='".$sdazl_code."',`dazl_importing`='".$sdazl_import."',`dtcc_nscc_id`='".$sdtcc_nscc."',`clearing_firm_id`='".$sclr_firm."'".$this->update_common_sql()." WHERE `id`='".$id."'";
+					$q = "UPDATE `".SPONSOR_MASTER."` SET `name`='".strtoupper($sponsor_name)."',`address1`='".$saddress1."',`address2`='".$saddress2."',`city`='".$scity."',`state`='".$sstate."',`zip_code`='".$szip_code."',`email`='".$semail."',`website`='".$swebsite."',`general_contact`='".$sgeneral_contact."',`general_phone`='".$sgeneral_phone."',`operations_contact`='".$soperations_contact."',`operations_phone`='".$soperations_phone."',`dst_system_id`='".$sdst_system_id."',`dst_mgmt_code`='".$sdst_mgmt_code."',`dst_importing`='".$sdst_import."',`dazl_code`='".$sdazl_code."',`dazl_importing`='".$sdazl_import."',`dtcc_nscc_id`='".$sdtcc_nscc."',`clearing_firm_id`='".$sclr_firm."'".$this->update_common_sql()." WHERE `id`='".$id."'";
                     $res = $this->re_db_query($q);
 					if($res)
           {
@@ -111,7 +111,7 @@
 			$date = isset($data['date'])?$this->re_db_input($data['date']):'';
             $user_id = isset($data['user_id'])?$this->re_db_input($data['user_id']):'';
             $sponsor_note = isset($data['sponsor_note'])?$this->re_db_input($data['sponsor_note']):'';
-            
+
             if($sponsor_note==''){
 				$this->errors = 'Please enter notes.';
 			}
@@ -122,7 +122,7 @@
                 if($notes_id==0){
                     $q = "INSERT INTO `".SPONSOR_NOTES."` SET `date`='".$date."',`user_id`='".$user_id."',`notes`='".$sponsor_note."'".$this->insert_common_sql();
 			        $res = $this->re_db_query($q);
-                    
+
                     $notes_id = $this->re_db_insert_id();
     				if($res){
     				    $_SESSION['success'] = INSERT_MESSAGE;
@@ -134,10 +134,10 @@
     				}
     			}
     			else if($notes_id>0){
-    			    
+
                     $q = "UPDATE `".SPONSOR_NOTES."` SET `date`='".$date."',`user_id`='".$user_id."',`notes`='".$sponsor_note."'".$this->update_common_sql()." WHERE `id`='".$notes_id."'";
        				$res = $this->re_db_query($q);
-                    
+
                     if($res){
     				    $_SESSION['success'] = UPDATE_MESSAGE;
     					return true;
@@ -151,14 +151,14 @@
 		}
         public function select_notes(){
 			$return = array();
-			
+
 			$q = "SELECT `s`.*
 					FROM `".SPONSOR_NOTES."` AS `s`
                     WHERE `s`.`is_delete`='0'
                     ORDER BY `s`.`id` ASC";
 			$res = $this->re_db_query($q);
             if($this->re_db_num_rows($res)>0){
-                
+
                 $a = 0;
     			while($row = $this->re_db_fetch_array($res)){
     			     array_push($return,$row);
@@ -191,9 +191,9 @@
             $user_id = isset($data['user_id'])?$this->re_db_input($data['user_id']):'';
             $file = isset($_FILES['file_attach'])?$_FILES['file_attach']:array();
             $valid_file = array('png','jpg','jpeg','bmp','pdf','xls','txt','xlsx');
-            
+
                 $attachment = $file;
-                $file = ''; 
+                $file = '';
                 $file_name = isset($attachment['name'])?$attachment['name']:'';
                 $tmp_name = isset($attachment['tmp_name'])?$attachment['tmp_name']:'';
                 $error = isset($attachment['error'])?$attachment['error']:0;
@@ -210,7 +210,7 @@
                 else
                 {
                     if($file_name!='')
-                    {            
+                    {
                        if(!in_array($ext,$valid_file))
                        {
                            $this->errors = 'Please select valid file.';
@@ -220,11 +220,11 @@
                            $attachment_file = time().rand(100000,999999).'.'.$ext;
                            move_uploaded_file($tmp_name,$target_dir.$attachment_file);
                            $file = $attachment_file;
-                           
+
                            if($attach_id==0){
                                 $q = "INSERT INTO `".SPONSOR_ATTACH."` SET `date`='".$date."',`user_id`='".$user_id."',`attach`='".$file."' ,`file_name`='".$file_name."'".$this->insert_common_sql();
                     	        $res = $this->re_db_query($q);
-                                
+
                                 $attach_id = $this->re_db_insert_id();
                     			if($res){
                     			    $_SESSION['success'] = INSERT_MESSAGE;
@@ -236,10 +236,10 @@
                     			}
                     		}
                     		else if($attach_id>0){
-                    		    
+
                                 $q = "UPDATE `".SPONSOR_ATTACH."` SET `date`='".$date."',`user_id`='".$user_id."',`attach`='".$file."' ,`file_name`='".$file_name."'".$this->update_common_sql()." WHERE `id`='".$attach_id."'";
                     			$res = $this->re_db_query($q);
-                                
+
                                 if($res){
                     			    $_SESSION['success'] = UPDATE_MESSAGE;
                     				return true;
@@ -250,20 +250,20 @@
                     			}
                     		}
                         }
-                   
-                }	
+
+                }
             }
 		}
         public function select_attach(){
 			$return = array();
-			
+
 			$q = "SELECT `s`.*
 					FROM `".SPONSOR_ATTACH."` AS `s`
                     WHERE `s`.`is_delete`='0'
                     ORDER BY `s`.`id` ASC";
 			$res = $this->re_db_query($q);
             if($this->re_db_num_rows($res)>0){
-                
+
                 $a = 0;
     			while($row = $this->re_db_fetch_array($res)){
     			     array_push($return,$row);
@@ -296,7 +296,7 @@
 		 * */
         public function select_state(){
 			$return = array();
-			
+
 			$q = "SELECT `s`.*
 					FROM `".STATE_MASTER."` AS `s`
                     WHERE `s`.`is_delete`='0' AND `s`.`status`='1'
@@ -306,14 +306,14 @@
                 $a = 0;
     			while($row = $this->re_db_fetch_array($res)){
     			     array_push($return,$row);
-                     
+
     			}
             }
 			return $return;
 		}
         public function get_previous_sponsor($id){
 			$return = array();
-			
+
             $q = "SELECT `at`.*
 					FROM `".SPONSOR_MASTER."` AS `at`
                     WHERE `at`.`is_delete`='0' and `at`.`id`<".$id."
@@ -327,10 +327,10 @@
                 return false;
             }
 			return $return;
-		} 
+		}
         public function get_next_sponsor($id){
 			$return = array();
-			
+
             $q = "SELECT `at`.*
 					FROM `".SPONSOR_MASTER."` AS `at`
                     WHERE `at`.`is_delete`='0' and `at`.`id`>".$id."
@@ -355,7 +355,7 @@
             if($this->re_db_num_rows($res)>0){
     			while($row = $this->re_db_fetch_array($res)){
     			     array_push($return,$row);
-                     
+
     			}
             }
 			return $return;
@@ -373,7 +373,7 @@
 		}
         public function select_sponsor(){
 			$return = array();
-			
+
 			$q = "SELECT `at`.*
 					FROM `".SPONSOR_MASTER."` AS `at`
                     WHERE `at`.`is_delete`='0'
@@ -383,15 +383,15 @@
                 $a = 0;
     			while($row = $this->re_db_fetch_array($res)){
     			     array_push($return,$row);
-                     
+
     			}
             }
 			return $return;
 		}
-    
+
 		public function select_sponsor_by_id($id){
 			$return = array();
-			
+
 			$q = "SELECT `sm`.*"
 					." FROM `".SPONSOR_MASTER."` AS `sm`"
 					." WHERE `sm`.`is_delete`='0'"
@@ -402,14 +402,14 @@
 
 			if($this->re_db_num_rows($res)>0) {
 				$return = $this->re_db_fetch_array($res);
-			}	
+			}
 
 			return $return;
 		}
-    
+
 		public function get_sponsor_on_system_management_code($system_id='',$management_code=''){
 			$return = array();
-			
+
 			$con = '';
 			if($system_id!='')
 			{
@@ -419,7 +419,7 @@
 			{
 				$con .= " AND `sp`.`dst_mgmt_code`='".$management_code."'";
 			}
-			
+
 			$q = "SELECT `sp`.*
 					FROM `".SPONSOR_MASTER."` AS `sp`
 					WHERE `sp`.`is_delete`='0' ".$con."
@@ -429,7 +429,7 @@
 				$a = 0;
 				while($row = $this->re_db_fetch_array($res)){
 						$return = $row;
-						
+
 				}
 			}
 			return $return;
@@ -443,7 +443,7 @@
 			} else if($search_text!='' && $search_text>=0){
 				$con .= " AND `clm`.`name` LIKE '%".$search_text."%' ";
 			}
-            
+
             $q = "SELECT `clm`.*
 					FROM `".SPONSOR_MASTER."` AS `clm`
                     WHERE `clm`.`is_delete`='0' "
@@ -466,7 +466,7 @@
         public function edit_sponsor($id){
 			$return = array();
 			$id = (int)$this->re_db_input($id);
-			
+
 			$q = "SELECT `at`.*"
 					." FROM `".SPONSOR_MASTER."` AS `at`"
                     ." WHERE `at`.`is_delete`='0'"
@@ -483,7 +483,7 @@
 		 * @param status to update
 		 * @return true if success, false message if any errors
 		 * */
-		
+
         public function sponsor_status($id,$status){
 			$id = trim($this->re_db_input($id));
 			$status = trim($this->re_db_input($status));
@@ -504,8 +504,8 @@
 				return false;
 			}
 		}
-		
-		
+
+
 		/**
 		 * @param id of record
 		 * @return true if success, false message if any errors
@@ -529,6 +529,17 @@
 				return false;
 			}
 		}
-        
+		
+		/** 05/07/22 For the "Transaction " button on the Maintain Sponsor form
+		 * @param mixed $id 
+		 * @return array of trades
+		 */
+		public function sponsor_transactions($id, $end_date=''){
+			$id = (int)$this->re_db_input($id);
+			// Only show the trades for the last year to limit the data set
+			if (){
+				
+			}
+		}
     }
 ?>
